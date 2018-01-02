58f07ffc0d12fa4963da2e5f2b86b92098643e40
Ciro S. Costa <ciro.costa@liferay.com>
2018-01-02 09:57:01 -0200

    Adds real file uplaoding capabilities to the H2 client; updates readme

--- README.md
+++ README.md
@@ -8,3 +8,27 @@
 
 `gupload` is an experiment to verify how uploading files via gRPC compares to a barebones HTTP2 server.
 
+#### HTTP2 server and client
+
+```sh
+# Start the HTTP2 server making use of HTTP2
+# with the TLS configuration using the key
+# and certificate from ./certs
+gupload serve \
+        --port 1313 \
+        --http2 \
+        --key ./certs/localhost.key \
+        --certificate ./certs/localhost.cert
+
+
+# Perform the upload of the file `./main.go` to the 
+# server at `localhost:1313` via HTTP2 appending
+# the self-signed certificate from `certs` to the
+# root CAs.
+gupload upload \
+        --http2 \
+        --address localhost:1313 \
+        --root-certificate ./certs/localhost.cert \
+        --file ./main.go
+```
+
--- core/http2_client.go
+++ core/http2_client.go
@@ -1,8 +1,10 @@
 package core
 
 import (
+	"bytes"
 	"crypto/tls"
 	"crypto/x509"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -16,7 +18,8 @@ import (
 // uploader that streams data via an HTTP2-enabled
 // connection.
 type ClientH2 struct {
-	client *http.Client
+	client  *http.Client
+	address string
 }
 
 type ClientH2Config struct {
@@ -59,12 +62,15 @@ func NewClientH2(cfg ClientH2Config) (c ClientH2, err error) {
 		},
 	}
 
+	c.address = cfg.Address
+
 	return
 }
 
 func (c *ClientH2) UploadFile(ctx context.Context, f string) (err error) {
 	var (
 		file *os.File
+		body = new(bytes.Buffer)
 	)
 
 	file, err = os.Open(f)
@@ -74,6 +80,34 @@ func (c *ClientH2) UploadFile(ctx context.Context, f string) (err error) {
 		return
 	}
 	defer file.Close()
+
+	_, err = io.Copy(body, file)
+	if err != nil {
+		err = errors.Wrapf(err,
+			"failed to copy file %s to body buffer",
+			f)
+		return
+	}
+
+	req, err := http.NewRequest("POST", c.address+"/upload", body)
+	if err != nil {
+		err = errors.Wrapf(err,
+			"failed to create POST request")
+		return
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		err = errors.Wrapf(err,
+			"request failed")
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		err = errors.Errorf("request failed - status code: %d",
+			resp.StatusCode)
+		return
+	}
 
 	return
 }
